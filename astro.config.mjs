31eb976a54b89c7aa0ce34c3acc7a714619347ea
Kayno0o <kevyn.fyleyssant@gmail.com>
2025-06-17 09:00:33 +0200

    feat: replace unocss with tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,34 @@
 import node from '@astrojs/node'
+import tailwindcss from '@tailwindcss/vite'
+
+import icon from 'astro-icon'
+
 // @ts-check
 import { defineConfig } from 'astro/config'
 
-import UnoCSS from 'unocss/astro'
 import { loadEnv } from 'vite'
 
 const { APP_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), '')
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [UnoCSS({
-    injectReset: true,
-  })],
   output: 'server',
+
   adapter: node({
     mode: 'standalone',
   }),
+
   devToolbar: {
     enabled: true,
   },
+
   vite: {
     server: {
       allowedHosts: [APP_URL],
     },
+
+    plugins: [tailwindcss()],
   },
+
+  integrations: [icon()],
 })
